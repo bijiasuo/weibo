52177999e3a7bc64025f6588c7e5bd049f085152
bgx <bicassos@outlook.com>
2016-01-28 23:58:51 +0800

    init

---
+++ test/test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by bgx on 16/1/28.
+//  Copyright © 2016年 bgx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+//  提交了新版本
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
